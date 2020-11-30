8f78f384c87eef248e02b8fa20fda5be8e16a259
小王子 <57303754+wangzi190@users.noreply.github.com>
2020-11-29 19:25:28 -0800

    Update animation.js
    update according to links update (fast.io to github storage)

--- animation.js
+++ animation.js
@@ -1,78 +1,66 @@
-var pageName;
+      var pageName;
 
-var darkenButton = function(id) {
-  var darkenSource = "https://c190stash.imfast.io/" + id + "1.png";
-        document.getElementById(id).src = darkenSource;
-};
+      var darkenButton = function(id) {
+        document.getElementById(id).style.filter = "brightness(82%)";
+      };
 
-var returnButton = function(id) {
-  if (id != pageName) {
-    var returnSource = "https://c190stash.imfast.io/" + id + ".png";
-    document.getElementById(id).src = returnSource;
-  } else {
-    var darkenSource = "https://c190stash.imfast.io/" + id + "1.png";
-    document.getElementById(id).src = darkenSource;
-  }
-};
+      var returnButton = function(id) {
+        if (id != pageName) {
+          document.getElementById(id).style.filter = "brightness(100%)";
+        } else {
+          document.getElementById(id).style.filter = "brightness(82%)";
+        }
+      };
 
-var darkenView = function(id) {
-  var darkenSource = "https://c190stash.imfast.io/" + document.getElementById(id).className + "1.png";
-  document.getElementById(id).src = darkenSource;
-}
-var returnView = function(id) {
-  var returnSource = "https://c190stash.imfast.io/" + document.getElementById(id).className + ".png";
-  document.getElementById(id).src = returnSource;
-}
+      function switchMain(id) {
+        var buttonNames = ["about", "links", "works", "contact"];
+        buttonNames = buttonNames.filter(e => e !== id);
+        function buttonHideButton() {
+          var i = 2;
+          while (i > -1) {
+            document.getElementById(buttonNames[i]).style.filter = "brightness(100%)";
+            i--;
+          }
+        }
+        switch(id) {
+          case "about":
+            buttonHideButton();
+            document.getElementById("back").hidden = false;
+            pageName = "about";
+            document.getElementById("storm").src = "https://user-images.githubusercontent.com/57303754/100564842-f9002c80-3276-11eb-8d62-041320521884.png";
+            document.getElementById("storm").alt = "hello! I'm Gray, a high school student with a passion for drawing & coding ^__^";
+          break;
 
-function switchMain(id) {
-  var buttonNames = ["about", "links", "works", "contact"];
-  buttonNames = buttonNames.filter(e => e !== id);
-  function buttonHideButton() {
-    var i = 2;
-    while (i > -1) {
-      document.getElementById(buttonNames[i]).src = "https://c190stash.imfast.io/" + buttonNames[i] + ".png";
-      i--;
-    }
-  }
-  switch(id) {
-    case "about":
-      buttonHideButton();
-      document.getElementById("back").hidden = false;
-      pageName = "about";
-      document.getElementById("storm").src = "https://c190stash.imfast.io/hello.png";
-      document.getElementById("storm").alt = "hello! I'm Gray, a high school student with a passion for drawing & coding ^__^";
-    break;
+          case "links":
+            buttonHideButton();
+            document.getElementById("back").hidden = true;
+            pageName = "links";
+            document.getElementById("storm").src = "https://user-images.githubusercontent.com/57303754/100564216-3f548c00-3275-11eb-976b-fca49ff1f360.png";
+            document.getElementById("storm").alt = "storm pagedoll";
+          break;
 
-    case "links":
-      buttonHideButton();
-      document.getElementById("back").hidden = true;
-      pageName = "links";
-      document.getElementById("storm").src = "https://c190draws.imfast.io/storm_pagedoll.png";
-      document.getElementById("storm").alt = "storm pagedoll";
-    break;
+          case "works":
+            buttonHideButton();
+            document.getElementById("back").hidden = false;
+            pageName = "works";
+            document.getElementById("storm").src = "https://user-images.githubusercontent.com/57303754/100564945-36fd5080-3277-11eb-9a64-c2092a0e8d97.png";
+            document.getElementById("storm").alt = "coming soon :)";
+          break;
+                
+          case "contact":
+            buttonHideButton();
+            document.getElementById("back").hidden = false;
+            pageName = "contact";
+            document.getElementById("storm").src = "https://user-images.githubusercontent.com/57303754/100564905-1af9af00-3277-11eb-8e6b-974df7224b14.png";
+            document.getElementById("storm").alt = "my email is c190draws@gmail.com and my discord tag is 小王子#0164";
+          break;
+        }
+      };
 
-    case "works":
-      buttonHideButton();
-      document.getElementById("back").hidden = false;
-      pageName = "works";
-      document.getElementById("storm").src = "https://c190stash.imfast.io/coming_soon.png";
-      document.getElementById("storm").alt = "coming soon :)";
-    break;
-          
-    case "contact":
-      buttonHideButton();
-      document.getElementById("back").hidden = false;
-      pageName = "contact";
-      document.getElementById("storm").src = "https://c190stash.imfast.io/contact_icons.png";
-      document.getElementById("storm").alt = "my email is c190draws@gmail.com and my discord tag is 小王子#0164";
-    break;
-  }
-};
-
-function returnMain() {
-  document.getElementById("storm").src = "https://c190draws.imfast.io/storm_pagedoll.png";
-  document.getElementById("storm").alt = "storm pagedoll";
-  document.getElementById(pageName).src = "https://c190stash.imfast.io/" + pageName + ".png";
-  document.getElementById("back").hidden = true;
-  pageName = "index";
-};
+      function returnMain() {
+        document.getElementById("storm").src = "https://user-images.githubusercontent.com/57303754/100564216-3f548c00-3275-11eb-976b-fca49ff1f360.png";
+        document.getElementById("storm").alt = "storm pagedoll";
+        document.getElementById(pageName).style.filter = "brightness(100%)";
+        document.getElementById("back").hidden = true;
+        pageName = "index";
+      };
